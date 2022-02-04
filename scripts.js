723651d676ebbb2aa92afc6810981a1350fa8f03
Rafael Perger <rafael.perger@gmail.com>
2022-02-04 17:59:27 +0100

    Add clear button.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,18 @@ const container = document.querySelector('#container');
 
 makeGrid(16, 16);
 
+const gridItems = document.querySelectorAll('.items')
+gridItems.forEach((item) => {
+    item.addEventListener('mouseover', (e) => {
+        item.style.backgroundColor = 'black';
+        })
+})
+
 function makeGrid(rows, cols) {
     for (i = 0; i < rows * cols; i++) {
         let gridItem = document.createElement('div');
-        gridItem.textContent = i + 1;
         gridItem.className = 'items';
+        gridItem.id = `item-${i+1}`;
 
         container.appendChild(gridItem)
     }
