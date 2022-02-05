d4e28f879e722e63953b1ecc5f7301f67d36456c
Rafael Perger <rafael.perger@gmail.com>
2022-02-05 12:12:19 +0100

    Creat pencil lifting feature.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
 const container = document.querySelector('#container');
+let canDraw = true;
 
 makeGrid(16, 16);
 
 addInk();
 addShade();
 
+const helpButton = document.getElementById('help');
+helpButton.addEventListener('click', () => {
+    alert("You can draw with your mouse.\nYou can lift up pencil with up arrow key.\nYou can put down pencil with down arrow key.")
+})
 
-const clearButton = document.querySelector('#clear-button')
-clearButton.addEventListener('click', clearPad)
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearPad);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+        canDraw = false;
+        addShade();
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowDown') {
+        canDraw = true;
+        addShade();
+    }
+})
 
 function makeGrid(rows, cols) {
     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
@@ -41,17 +60,19 @@ function addInk() {
 
 function addShade() {
     let gridItems = document.querySelectorAll('.items');
-    gridItems.forEach((item) => {
-        item.addEventListener('mouseover', (e) => {
-            let splitBGColor = item.style.backgroundColor.split(/\(| |\)/);
-
-            let red = splitBGColor[1].slice(0, -1);
-            let green = splitBGColor[2].slice(0, -1);
-            let blue = splitBGColor[3]
-
-            item.style.backgroundColor = `rgb(${red - 22.5}, ${green - 22.5}, ${blue - 22.5})`;
-            })
-    })
+        gridItems.forEach((item) => {
+            item.addEventListener('mouseover', (e) => {
+                if (canDraw === true) {
+                    let splitBGColor = item.style.backgroundColor.split(/\(| |\)/);
+        
+                    let red = splitBGColor[1].slice(0, -1);
+                    let green = splitBGColor[2].slice(0, -1);
+                    let blue = splitBGColor[3]
+        
+                    item.style.backgroundColor = `rgb(${red - 22.5}, ${green - 22.5}, ${blue - 22.5})`;
+                }
+            }) 
+        })  
 }
 
 function isInputValid (input) {
